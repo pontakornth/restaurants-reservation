391f6f1a203023f51bcf0073f4cfc10634deb91a
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2022-02-06 15:19:19 +0700

    feat: Implemented reservation by name

--- main.py
+++ main.py
@@ -2,40 +2,49 @@ from fastapi import FastAPI
 from pymongo import MongoClient
 from pydantic import BaseModel
 
+
 class Reservation(BaseModel):
-    name : str
+    name: str
     time: int
     table_number: int
-    
+
+
 client = MongoClient('mongodb://localhost', 27017)
 
-# TODO fill in database name
-db = client["<put your database name>"]
+db = client["exceed_restaurant"]
 
-# TODO fill in collection name
-collection = db["<put your collection name>"]
+collection = db["reservation"]
 
 app = FastAPI()
 
 
 # TODO complete all endpoint.
 @app.get("/reservation/by-name/{name}")
-def get_reservation_by_name(name:str):
-    pass
+def get_reservation_by_name(name: str):
+    result = collection.find({'name': name}, {'_id': 0})
+    reservation_list = []
+    for r in result:
+        reservation_list.append(r)
+    return {
+        'reservations': reservation_list
+    }
+
 
 @app.get("reservation/by-table/{table}")
 def get_reservation_by_table(table: int):
     pass
 
+
 @app.post("/reservation")
-def reserve(reservation : Reservation):
+def reserve(reservation: Reservation):
     pass
+
 
 @app.put("/reservation/update/")
 def update_reservation(reservation: Reservation):
     pass
 
-@app.delete("/reservation/delete/{name}/{table_number}")
-def cancel_reservation(name: str, table_number : int):
-    pass
 
+@app.delete("/reservation/delete/{name}/{table_number}")
+def cancel_reservation(name: str, table_number: int):
+    pass
